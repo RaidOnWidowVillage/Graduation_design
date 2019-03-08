b9abcd7060039b1edc24630669df1685cc4eef61
jayneLiu <ljy6327@vip.qq.com>
2019-03-08 11:09:50 +0800

    修改数据库实体类表

--- src/main/java/com/qiancheng/charging/entity/User.java
+++ src/main/java/com/qiancheng/charging/entity/User.java
@@ -8,7 +8,7 @@ public class User {
     private String password; //密码
     private int role; //角色 0普通用户 1管理员
     private int flag;
-    private String position; //职位
+    private String employeeId; //员工id
     private Date createTime;
     private Date updateTime;
     private String createUser;
@@ -19,13 +19,13 @@ public class User {
 
     }
 
-    public User(String id, String username, String password, int role, int flag, String position, Date createTime, Date updateTime, String createUser, String updateUser, Date lastLoginTime) {
+    public User(String id, String username, String password, int role, int flag, String employeeId, Date createTime, Date updateTime, String createUser, String updateUser, Date lastLoginTime) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.role = role;
         this.flag = flag;
-        this.position = position;
+        this.employeeId = employeeId;
         this.createTime = createTime;
         this.updateTime = updateTime;
         this.createUser = createUser;
@@ -73,12 +73,12 @@ public class User {
         this.flag = flag;
     }
 
-    public String getPosition() {
-        return position;
+    public String getEmployeeId() {
+        return employeeId;
     }
 
-    public void setPosition(String position) {
-        this.position = position;
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
     }
 
     public Date getCreateTime() {
--- src/main/java/com/qiancheng/charging/login/dao/UserDao.java
+++ src/main/java/com/qiancheng/charging/login/dao/UserDao.java
@@ -11,19 +11,19 @@ import java.util.List;
 
 @MyBatisDao
 public interface UserDao {
-    @Insert("insert into hr_user(id,username,password,role,flag,position,createtime,updatetime,createuser,updateuser,last_login_time) values(UUID(),#{username},#{password},1,1,1,now(),now(),#{createUser},#{updateUser},now())")
+    @Insert("insert into hr_user(id,username,password,role,flag,employeeId,createtime,updatetime,createuser,updateuser,last_login_time) values(UUID(),#{username},#{password},1,1,1,now(),now(),#{createUser},#{updateUser},now())")
     Integer insert_user(User user);
 
-    @Select("SELECT id,username,password,role,flag,position,createtime,updatetime,createuser,updateuser,last_login_time as lastLoginTime from hr_user where username = #{username}")
+    @Select("SELECT id,username,password,role,flag,employeeId,createtime,updatetime,createuser,updateuser,last_login_time as lastLoginTime from hr_user where username = #{username}")
     User selectUserByUsername(User user);
 
     @Update("update hr_user set last_login_time=now() where id = #{id}")
     Integer updateLastLoginTime(String id);
 
-    @Select("SELECT id,username,password,role,flag, position from hr_user where (role=#{role} or role is null) and (username like '%${username}%' or username is null)")
+    @Select("SELECT id,username,password,role,flag, employeeId from hr_user where (role=#{role} or role is null) and (username like '%${username}%' or username is null)")
     List<User> selectAllUser(User user);
 
-    @Select("SELECT id,username,password,role,flag,position from hr_user  where id=#{id}")
+    @Select("SELECT id,username,password,role,flag,employeeId from hr_user  where id=#{id}")
     User selectUserById(String id);
 
     @Update("update hr_user set username = #{username}, password = #{password}, role = #{role}, flag = #{flag}, position = #{position} where id = #{id}")
